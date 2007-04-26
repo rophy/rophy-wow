ae3f01b2b81c9f1ebcd904867caa9c808014049f
rophy123 <rophy123@d01c0743-7126-0410-bc3a-e5937afe6038>
2007-04-26 01:55:30 +0000

    KombatMeters: - RaidUnits: Make it a proper library, with self-upgrading.

--- trunk/KombatMeters/RaidUnits.lua
+++ trunk/KombatMeters/RaidUnits.lua
@@ -1,18 +1,83 @@
---[[
-	Maintain a table of the raid names.
+--[[---------------------------------------------------------------------------
+	
+	RaidUnits : A tiny library which maps names to unitids.
+	
+	Author: Rophy
+	
+	API:
+		
+		unitid = RaidUnits:GetUnitID("name") 
+			- Get unitid from name, nil if the name is not raid member.
+		
+		unitid = RaidUnits("name") 
+			- Shortcut for RaidUnits:GetUnitID("name").
+		
+		RaidUnits:Refresh() 
+			- Manual refresh unitid table, you shouldn't need to call this.
+		
+-----------------------------------------------------------------------------]]
 
-]]
+local version = tonumber(string.match("$Revision$", "(%d+)") or 1)
 
-local addonName = "KombatMeters_RaidUnits"
+local RaidUnits
 
-RaidUnits = DongleStub("Dongle-1.0"):New(addonName)
+local frame
+local units
+local timeToRefresh
 
-local units = {}
+if _G.RaidUnits then
+	local oldLib = _G.RaidUnits
+	if oldLib.version >= version then
+		-- There is already a newer version.
+		return
+	else
+		-- Upgrading, use the old table so that old references still works.
+		frame = oldLib.frame
+		for k in pairs(oldLib) do oldLib[k] = nil end
+		RaidUnits = oldLib
+	end
+else
+	RaidUnits = {}
+end
 
-function RaidUnits:Enable()
-	self:RegisterEvent("RAID_ROSTER_UPDATE", "Refresh")
-	self:RegisterEvent("PARTY_MEMBERS_CHANGED", "Refresh")
-	self:RegisterEvent("UNIT_PET", "Refresh")
+
+function OnLoad()
+	-- Make RaidUnits("name") as a short cut for RaidUnits:GetUnitID("name").
+	setmetatable(RaidUnits, { 
+		__call = function(t,k)
+			return t:GetUnitID(k)
+		end 
+	} )
+	
+	units = {}
+	if not frame then
+		frame = CreateFrame("Frame")
+	end
+	frame:Hide()
+	frame:SetScript("OnEvent", OnEvent)
+	frame:SetScript("OnUpdate", OnUpdate)
+	frame:UnregisterAllEvents()
+	frame:RegisterEvent("RAID_ROSTER_UPDATE")
+	frame:RegisterEvent("PARTY_MEMBERS_CHANGED")
+	frame:RegisterEvent("UNIT_PET")
+	frame:RegisterEvent("PLAYER_ENTERING_WORLD")
+	RaidUnits.units = units
+	RaidUnits.frame = frame
+	_G["RaidUnits"] = RaidUnits
+end
+
+function OnEvent(frame,event)
+	timeToRefresh = 0
+	frame:Show()
+end
+
+function OnUpdate(this,elapsed)
+	timeToRefresh = timeToRefresh + elapsed
+	if timeToRefresh > 0.2 then
+		RaidUnits:Refresh()
+		this:Hide()
+		timeToRefresh = 0
+	end
 end
 
 function RaidUnits:Refresh()
@@ -30,6 +95,7 @@ function RaidUnits:Refresh()
 			units[name] = "raid"..i.."pet"
 		end
 	end
+	
 	local partySize = GetNumPartyMembers()
 	for i=1, partySize do
 		local name = UnitName("party"..i)
@@ -47,12 +113,11 @@ function RaidUnits:Refresh()
 	if name then
 		units[name] = "pet"
 	end
+	
 end
 
 function RaidUnits:GetUnitID(name)
-	if name == UnitName("player") then
-		return "player"
-	else
-		return units[name]
-	end
+	return units[name]
 end
+
+OnLoad()
