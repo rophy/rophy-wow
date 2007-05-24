942483fb781b6a88b28681c98eaed4c296f448cc
rophy123 <rophy123@d01c0743-7126-0410-bc3a-e5937afe6038>
2007-05-24 05:00:17 +0000

    rOmniCC: bug fix.

--- trunk/OmniCC/basic.lua
+++ trunk/OmniCC/basic.lua
@@ -20,19 +20,12 @@ local colors = {
 local function GetFormattedTime(secs)
 
 	if secs >= 86400 then
-
 		return floor(secs / 86400 + 0.5), mod(secs, 86400), 86400
-
 	elseif secs >= 3600 then
-
 		return floor(secs / 3600 + 0.5), mod(secs, 3600), 3600
-
 	elseif secs >= 60 then
-
 		return floor(secs / 60 + 0.5), mod(secs, 60), 60
-
 	end
-
 	return floor(secs + 0.5), secs - floor(secs), false
 
 end
@@ -43,12 +36,12 @@ local function Timer_OnUpdate(self, elapsed)
 			local remain = self.duration - (GetTime() - self.start)
 			if floor(remain + 0.5) > 0 then
 				local time, toNextUpdate, toNextColor = GetFormattedTime(remain)
-				if self.toNextColor == nil or (self.toNextColor and remain < self.toNextColor) then
+				if self.toNextColor and remain < self.toNextColor then
 					self.text:SetTextColor(unpack(colors[toNextColor]))
+					self.toNextColor = toNextColor
 				end
 				self.text:SetText(time)
 				self.toNextUpdate = toNextUpdate
-				self.toNextColor = toNextColor
 			else
 				self.text:Hide()
 			end
@@ -78,6 +71,7 @@ local function Timer_Start(self, start, duration)
 	self.start = start
 	self.duration = duration
 	self.nextUpdate = 0
+	self.toNextColor = duration
 
 	local text = self.text or Timer_Create(self)
 	if text then
