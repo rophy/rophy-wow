12611e40a4aea849a452e30838a57003d880d09a
rophy123 <rophy123@d01c0743-7126-0410-bc3a-e5937afe6038>
2007-06-15 06:40:49 +0000

    SimpleBankState: - Tagging the original Ace2 version.

---
+++ tags/SimpleBankState/localization.lua
@@ -0,0 +1,35 @@
+
+SimpleBankState.loc = {
+
+	TITLE = "SimpleBankState",
+
+	KEYWORD = "Keyword: ",
+	REFRESH = "Refresh",
+
+	-- Field names.
+	ITEM_NAME = "Name",
+	RARITY = "Rarity",
+	QUANTITY = "Quantity",
+	PLAYER = "Player",
+	BAG_TYPE = "Type", -- Title for container types.	
+
+	-- item rarities.
+	POOR = "Poor",
+	NORMAL = "Normal",
+	GOOD = "Good",
+	RARE = "Rare",
+	EPIC = "Epic",
+	LEGENDARY = "Legendary",
+	ARTIFACT = "Artifact",
+	
+	-- container types.
+	BAG = "Bag",
+	BANK = "Bank",
+	EQUIPMENT = "Equip",
+	MAIL = "Mail",
+
+	
+
+}
+
+
