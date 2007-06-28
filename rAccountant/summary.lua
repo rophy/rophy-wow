51160d8489cc6c59587cf0dc66cbd2f1a2671e7f
rophy123 <rophy123@d01c0743-7126-0410-bc3a-e5937afe6038>
2007-06-28 04:41:39 +0000

    rAccountant - transaction: - implemented basic logics of summarization.

--- rAccountant/summary.lua
+++ rAccountant/summary.lua
@@ -21,3 +21,40 @@
 			
 
 ]]
+local transaction = Transaction
+
+if not transaction then return end
+
+Summary = DongleStub("Dongle-1.1"):New("rAccountant_Summary")
+
+--[[ Filters ]]--
+local beginTime, endTime
+local players = {}
+local categories = {}
+local server = transaction:GetCurrentServerName()
+
+function Summary:PlayerReport()
+	beginTime, endTime = time()-60*60*24*2, time()
+	
+	local income, expense = 0, 0
+	for player in transaction:IteratePlayers(server) do
+		if not players[player] then	-- player filter.
+			local from, to = transaction:SearchByTime(beginTime, endTime, player, server)
+			if from then
+				for i=from, to do
+					local cat,_,amount = transaction:GetData(i)
+					
+					if not categories[cat] then	-- category filter.
+						if amount > 0 then
+							income = income + amount
+						else
+							expense = expense - amount
+						end
+					end
+				end
+			end
+			self:Print(player,income,expense,income-expense)
+		end
+	end
+end
+
